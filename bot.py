580e90af1c29c0db04185bcf5b61ee181c195c66
Linus Jansson <linus.jansson2000@gmail.com>
2021-11-19 23:06:33 +0100

    Added basic kick on filter failed

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3.8
 import os
 import discord
 from discord.ext import commands
@@ -5,6 +6,8 @@ from discord.ext.commands import has_permissions, MissingPermissions
 import typing
 import time
 from dotenv import load_dotenv
+# from profanity_check import predict, predict_prob
+
 
 load_dotenv('.env')
 
@@ -18,13 +21,27 @@ game = discord.Game("NULL")
 verifed_users = [533582968371806211, 348406262792323072]
 privilaged_users = [322015089529978880]
 
+uganda = 525442477365133323
+
 
 async def is_owner(ctx):
     return ctx.author.id in privilaged_users
 
-async def filter(msg):
+async def passedFilter(msg):
     passed = True
+    msg = msg.split()
     # Filters messages
+    # with open("wordlist.txt", 'r') as f:
+    #     words = f.read()
+    #     badwords = re.split('\"[a-zA-Z0-9]*\"', words)
+    #     print(badwords)
+    # print(predict_prob([msg]))
+    print("filtering")
+
+    for c in msg:
+        if c == "neger" or c == 'nigger':
+            passed = False
+            break
 
     return passed
 
@@ -37,19 +54,22 @@ async def on_ready():
 
 @client.event
 async def on_member_join(joined_member):
-    if joined_member.id in verifed_users:
+    if joined_member.id in verifed_users and joined_member.guild.id == uganda:
         knuckle = discord.utils.get(joined_member.guild.roles, id=525444324561780737)
         await joined_member.edit(roles=[knuckle])
         print("ADDED ROLE TO ", joined_member)
-
 
 @client.event
 async def on_message(message):
     if message.author.bot:
         pass
     
-    if filter(message.content):
-        pass
+    if not await passedFilter(message.content):
+        await message.delete()
+        await message.author.kick()
+
+    
+    await client.process_commands(message)
 
 # @client.check
 # async def globally_block_dms(ctx):
@@ -100,6 +120,7 @@ async def testCount(ctx, amount: int):
     for n in range(1, amount - 1):
         await ctx.send(str(n))
         time.sleep(1)
+
         
 token = os.environ.get('TOKEN', 0)
 
