dee1ff773944563623cba042a79673d628d41117
Linus J <linus.jansson2000@gmail.com>
2021-11-23 21:36:51 +0100

    Changed banned words to a array in a seperate file

---
+++ wordlist.py
@@ -0,0 +1,13 @@
+bannedWords = [
+    "neger", 
+    "negrar", 
+    "n3g3r", 
+    "n3ger", 
+    "neg3r",
+    "nigger",
+    "svarting",
+    "taiwan",
+    "allah",
+    "destiny",
+    "tinamen"
+]
